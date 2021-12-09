f5c7b01f66a0e899c5e660e11d58db020efcbfcf
Jiya-Ull-Haq <Shaik.Jiyaullhaq@gmail.com>
2021-12-09 11:40:24 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/icaec'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/icaec/index.html'));});
+
+app.use(requireHTTPS);
+app.use(express.static('./dist/icaec'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: '/dist/icaec/'}),
+);
+
 app.listen(process.env.PORT || 8080);
