b4da15a4d77a78e7090936171b35dcc7b26c073d
Jiya-Ull-Haq <Shaik.Jiyaullhaq@gmail.com>
2021-12-09 12:27:08 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.get('/*', (req, res) =>
     {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'dist/icaec/', 'index.html'));
     }
     );
 
