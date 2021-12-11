65ef9b829e57c87df709006d305ef114cae4f5a0
Jiya <60106386+Jiya-Ull-Haq@users.noreply.github.com>
2021-12-11 20:57:19 +0530

    V2.0

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ParticlesConfig} from './particles-config';
+
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-home',
@@ -10,6 +13,13 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.invokeParticles();
+    particlesJS.load('particles-js', 'particles.json', null);
   }
+
+  public invokeParticles(): void {
+    particlesJS('particles-js', ParticlesConfig, function() {});
+  }
+
 
 }
