ac0919ea1c4c8b4c04b381f6e442c786c7978502
Jiya <60106386+Jiya-Ull-Haq@users.noreply.github.com>
2021-12-21 11:56:01 +0530

    team done. Yo mama

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl} from '@angular/forms';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  selected: any
+  
+
   constructor() { }
 
   ngOnInit(): void {
+
+     // http://localhost:4200/about#institute
+     let url = window.location.href;
+     let about_section = document.getElementById("about")
+
+     if(url.includes('/about?id=0')){
+       about_section?.scrollIntoView(true)
+       this.selected = new FormControl(0);
+     }
+     if(url.includes('/about?id=2')){
+      about_section?.scrollIntoView(true)
+       this.selected = new FormControl(2);
+     }
+     if(url.includes('/about?id=1')){
+      about_section?.scrollIntoView(true)
+       this.selected = new FormControl(1);
+     }
+     if(url.includes('/about')){
+      about_section?.scrollIntoView(true)
+      this.selected = new FormControl(1);
+     }
+
   }
 
+change_tab(){
+ 
 }
+
+}
+
+
