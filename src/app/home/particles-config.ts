e02b807902c52400c6def41be3ac21b23a015b97
Jiya <60106386+Jiya-Ull-Haq@users.noreply.github.com>
2021-12-21 10:43:23 +0530

    YoMama

--- src/app/home/particles-config.ts
+++ src/app/home/particles-config.ts
@@ -1,16 +1,16 @@
 export const ParticlesConfig = {
   particles: {
-    number: { value: 150, density: { enable: false, value_area: 20 } },
-    color: { value: "#000000" },
+    number: { value: 65, density: { enable: true, value_area: 900 } },
+    color: { value: "#ffffff" },
     shape: {
       type: "circle",
       stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 6 },
+      polygon: { nb_sides: 8 },
       image: { src: "img/github.svg", width: 100, height: 100 }
     },
     opacity: {
-      value: 0.5,
-      random: true,
+      value: 1,
+      random: false,
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
     },
     size: {
@@ -21,9 +21,9 @@ export const ParticlesConfig = {
     line_linked: {
       enable: true,
       distance: 150,
-      color: "#000000",
-      opacity: 0.4,
-      width: 1
+      color: "#ffffff",
+      opacity: 0.5,
+      width: 2.5
     },
     move: {
       enable: true,
@@ -40,13 +40,13 @@ export const ParticlesConfig = {
     detect_on: "window",
     events: {
       onhover: { enable: true, mode: "repulse" },
-      onclick: { enable: false, mode: "push" },
+      onclick: { enable: false, mode: "repulse" },
       resize: true
     },
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
+      repulse: { distance: 150, duration: 0.4 },
       push: { particles_nb: 4 },
       remove: { particles_nb: 2 }
     }
