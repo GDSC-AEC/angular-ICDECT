fe9613764a698fd55dcbec48fdb461ac419c38a6
Jiya <60106386+Jiya-Ull-Haq@users.noreply.github.com>
2022-01-17 13:44:15 +0530

    sound; form; data additional;

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,53 +27,73 @@ export class HomeComponent implements OnInit {
   public invokeParticles(): void {
     particlesJS('particles-js', ParticlesConfig, function() {});
   }
-
+  test(){
+    console.log(this.name);
+  }
   async sendEmail() {
 
-    let trigger = $('.sent-message');
+    let success_message = $('.sent-message');
     let form = $('.php-email-form');
-    let target = $('.NavBar');
     let loading = $('.loading');
 
-    loading.css({
-      "content": "",
-      "display": "block",
-      "border-radius": "50%",
-      "width": "24px",
-      "height": "24px",
-      "margin": "0 10px -6px 0",
-      "border": "3px solid #18d26e",
-      "border-top-color": "#eee",
-      "-webkit-animation": "animate-loading 1s linear infinite",
-      "animation": "animate-loading 1s linear infinite"
-      })
+    async function woohoo() {
+      let audio = new Audio();
+      audio.src = "assets/sounds/03_principle_informative-02.mp3";
+      audio.load();
+      audio.play();
+    }
 
-    Email.send({
-    Host: "smtp.gmail.com",
-    Username : "ICDECT2022@gmail.com",
-    Password : "@2022AEC",
-    To : 'ICDECT2022@aec.edu.in',
-    From : this.email,
-    Subject : this.subject,
-    Body :
-    ` Name: ${this.name}, \n
-      Email: ${this.email}, \n
-      Message: ${this.message}.\n
-    `
-    }).then(
-
-      async function dingdong() {
-        loading.css({
-          "display": "none"
+    if(this.name != undefined && this.email != undefined && this.subject != undefined && this.message != undefined){
+      loading.css({
+        "content": "",
+        "display": "block",
+        "border-radius": "50%",
+        "width": "24px",
+        "height": "24px",
+        "margin": "0 10px -6px 0",
+        "border": "3px solid #18d26e",
+        "border-top-color": "#eee",
+        "-webkit-animation": "animate-loading 1s linear infinite",
+        "animation": "animate-loading 1s linear infinite"
         })
-        trigger.css({
-          "display": "block"
-        })
-        form.trigger("reset");
 
-      }
+        Email.send({
+          Host: "smtp.gmail.com",
+          Username : "ICDECT2022@gmail.com",
+          Password : "@2022AEC",
+          // To : 'ICDECT2022@aec.edu.in', // Production
+          To : '19a91a05c2@aec.edu.in', // Development
+          From : this.email,
+          Subject : this.subject,
+          Body :
+          ` Name: ${this.name}, \n
+            Email: ${this.email}, \n
+            Message: ${this.message}.\n
+          `
+          }).then(
 
-    )
+            async function dingdong() {
+              loading.css({
+                "display": "none"
+              })
+              woohoo();
+              success_message.css({
+                "display": "block"
+              })
+              form.trigger("reset");
+
+
+            }
+          )
+
+        }
+        else{
+          alert('Something is missing.\nPlease Make Sure Nothing is Empty.');
+        }
+
+
+
+
 
 
   }
